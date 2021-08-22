3625f5b7ea773d9beb9083601fda27b33094dca9
Nicolas Musset <musset.nicolas@gmail.com>
2021-08-22 11:43:40 +0900

    Add a bench for the buffered version

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,6 +1,7 @@
 use criterion::{criterion_group, criterion_main, Criterion};
-use jsonformat::{format_json, Indentation};
-use std::{fs, io};
+use jsonformat::{format_json, format_json_buffered, Indentation};
+use std::fs;
+use std::io::{self, BufReader, BufWriter, Read, Write};
 
 /// You need a json file called massive.json in your project root
 fn format_massive_json(file: &str) -> io::Result<String> {
@@ -12,6 +13,30 @@ fn criterion_benchmark(c: &mut Criterion) {
 
     c.bench_function("Format massive json", |b| {
         b.iter(|| format_massive_json(&file))
+    });
+}
+
+fn format_massive_json_buffered<R, W>(
+    reader: &mut BufReader<R>,
+    writer: &mut BufWriter<W>,
+) -> io::Result<()>
+where
+    R: Read,
+    W: Write,
+{
+    format_json_buffered(reader, writer, Indentation::Default).unwrap();
+    Ok(())
+}
+
+fn criterion_benchmark_buffered(c: &mut Criterion) {
+    let file = fs::read_to_string("massive.json").expect("massive.json file in project directory");
+
+    c.bench_function("Format massive json", |b| {
+        b.iter(|| {
+            let mut reader = BufReader::new(file.as_bytes());
+            let mut writer = BufWriter::new(std::io::sink());
+            format_massive_json_buffered(&mut reader, &mut writer)
+        })
     });
 }
 
